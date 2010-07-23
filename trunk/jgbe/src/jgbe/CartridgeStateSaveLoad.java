de6ee6b67058f863708c3170ef7c7feb8bf5793c
seth.holth@gmail.com <seth.holth@gmail.com@c3224f74-1d76-11df-a646-7b274f648fea>
2010-07-23 21:40:32 +0000

    Extract the save/load functionality from the Cartridge class

---
+++ trunk/jgbe/src/jgbe/CartridgeStateSaveLoad.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package jgbe;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+class CartridgeStateSaveLoad {
+	/**
+	 * 
+	 */
+	private final Cartridge cartridge;
+
+	/**
+	 * @param cartridge
+	 */
+	CartridgeStateSaveLoad(Cartridge cartridge) {
+		this.cartridge = cartridge;
+	}
+
+	protected void stateSaveLoad(boolean save, int version, DataOutputStream dostream, DataInputStream distream) throws IOException {
+		boolean isnull = false;
+		for (int t = 0; t < Cartridge.MAX_RAM_MM; ++t) {
+			if ((save))
+				isnull = (this.cartridge.MM_RAM[t] == null);
+			{
+				if ((save))
+					dostream.writeBoolean(isnull);
+				else
+					isnull = distream.readBoolean();
+			}
+			;
+			if (!isnull) {
+				for (int sl_i = 0; sl_i < (Cartridge.MEMMAP_SIZE); ++sl_i) {
+					if ((save))
+						dostream.writeByte(((this.cartridge.MM_RAM[t][sl_i]) & 0xff));
+					else
+						this.cartridge.MM_RAM[t][sl_i] = (distream.readUnsignedByte());
+				}
+				;
+			} else if ((!save))
+				this.cartridge.MM_RAM[t] = null;
+		}
+
+		{
+			for (int sl_i = 0; sl_i < (0x100); ++sl_i) {
+				if ((save))
+					dostream.writeByte(((this.cartridge.BIOS_ROM[sl_i]) & 0xff));
+				else
+					this.cartridge.BIOS_ROM[sl_i] = (distream.readUnsignedByte());
+			}
+			;
+		}
+		;
+
+		{
+			if ((save))
+				dostream.writeBoolean(this.cartridge.ram_enabled);
+			else
+				this.cartridge.ram_enabled = distream.readBoolean();
+		}
+		;
+		{
+			if ((save))
+				dostream.writeBoolean(this.cartridge.RTCRegisterEnabled);
+			else
+				this.cartridge.RTCRegisterEnabled = distream.readBoolean();
+		}
+		;
+		{
+			if ((save))
+				dostream.writeInt((int) this.cartridge.RomRamModeSelect);
+			else
+				this.cartridge.RomRamModeSelect = distream.readInt();
+		}
+		;
+		{
+			if ((save))
+				dostream.writeInt((int) this.cartridge.CurrentROMBank);
+			else
+				this.cartridge.CurrentROMBank = distream.readInt();
+		}
+		;
+		{
+			if ((save))
+				dostream.writeInt((int) this.cartridge.CurrentRAMBank);
+			else
+				this.cartridge.CurrentRAMBank = distream.readInt();
+		}
+		;
+		{
+			if ((save))
+				dostream.writeInt((int) this.cartridge.CurrentRTCRegister);
+			else
+				this.cartridge.CurrentRTCRegister = distream.readInt();
+		}
+		;
+	}
+}
