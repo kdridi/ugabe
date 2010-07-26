90cc9c7097be9cbf03697d4b6543f0bd581455a0
seth.holth <seth.holth@c3224f74-1d76-11df-a646-7b274f648fea>
2010-07-26 12:20:39 +0000

    Setting up the license

--- jgbe/src/jgbe/CPULoadState.java
+++ jgbe/src/jgbe/CPULoadState.java
@@ -1,3 +1,18 @@
+/* ==========================================================================
+ * GNU GENERAL PUBLIC LICENSE
+ * Version 2, June 1991
+ * 
+ * Copyright (C) 1989, 1991 Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ * 
+ * $LastChangeDate$
+ * $Rev$
+ * $LastChangedBy$
+ * $URL$
+ * $Id$
+ * ========================================================================== */ 
 package jgbe;
 
 import java.io.DataInputStream;
@@ -10,7 +25,7 @@ class CPULoadState {
 		int loadversion;
 		int magix = distream.readInt();
 		if (magix != (0x4a374a53)) {
-			
+
 			loadversion = 0;
 			cpu.B = (magix >> 24) & 0xff;
 			cpu.C = (magix >> 16) & 0xff;
@@ -24,19 +39,19 @@ class CPULoadState {
 			throw new IOException("save state too new");
 		if (loadversion != (23))
 			System.out.println("loading state with old version:" + loadversion);
-		
+
 		int compressionmethod = 0;
 		if (loadversion >= 5)
 			compressionmethod = distream.readInt();
 		switch (compressionmethod) {
-		
+
 		case 0:
 			break;
-			
+
 		case 1:
 			distream = new DataInputStream(new GZIPInputStream(distream));
 			break;
-			
+
 		default:
 			throw new IOException("unknown compression method:" + compressionmethod);
 		}
--- jgbe/src/jgbe/CPUSaveState.java
+++ jgbe/src/jgbe/CPUSaveState.java
@@ -1,3 +1,18 @@
+/* ==========================================================================
+ * GNU GENERAL PUBLIC LICENSE
+ * Version 2, June 1991
+ * 
+ * Copyright (C) 1989, 1991 Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ * 
+ * $LastChangeDate$
+ * $Rev$
+ * $LastChangedBy$
+ * $URL$
+ * $Id$
+ * ========================================================================== */ 
 package jgbe;
 
 import java.io.DataOutputStream;
@@ -10,33 +25,33 @@ class CPUSaveState {
 		int saveversion = (23);
 		dostream.writeInt((0x4a374a53));
 		dostream.writeInt(saveversion);
-		
+
 		int compressionmethod = 1;
 		DeflaterOutputStream zostream = null;
-		
+
 		dostream.writeInt(compressionmethod);
 		switch (compressionmethod) {
-		
+
 		case 0:
 			break;
-			
+
 		case 1: {
 			zostream = new GZIPOutputStream(dostream);
 			dostream = new DataOutputStream(zostream);
 		}
-		;
-		break;
-		
+			;
+			break;
+
 		default:
 			if (!(false))
 				throw new Error("Assertion failed: " + "false");
 		}
 		StateSaveLoad.Impl.stateSaveLoad(true, saveversion, dostream, null, cpu);
-		
+
 		if (zostream != null) {
 			dostream.flush();
 			zostream.finish();
 		}
-		
+
 	}
 }
