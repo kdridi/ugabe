d73b72889d549d01bbb91b3b4b7f449d7cc0ba57
seth.holth@gmail.com <seth.holth@gmail.com@c3224f74-1d76-11df-a646-7b274f648fea>
2010-07-26 21:31:54 +0000

    Updating CartridgeController due to GWT compliance for the logging utility

--- trunk/jgbe/src/com/arykow/applications/ugabe/client/CartridgeController.java
+++ trunk/jgbe/src/com/arykow/applications/ugabe/client/CartridgeController.java
@@ -35,7 +35,7 @@ public class CartridgeController {
 			private Cartridge cartridge = new Cartridge();
 
 			public void onFailure(Throwable caught) {
-				System.out.println("error loading cartridge from file!: " + caught.toString());
+				CPULogger.log("error loading cartridge from file!: " + caught.toString());
 				cartridge.status = Cartridge.STATUS_FATAL_ERROR;
 				cartridge.err_msg = caught.getMessage();
 				if (caught instanceof EOFException) {
@@ -183,65 +183,65 @@ public class CartridgeController {
 			break;
 		default:
 			cartridge.MBC = -666;
-			System.out.println("*UNKNOWN*");
+			CPULogger.log("*UNKNOWN*");
 			throw new java.io.IOException("unknown MBC type");
 		}
-		System.out.println(" (MBC" + cartridge.MBC + ")");
+		CPULogger.log(" (MBC" + cartridge.MBC + ")");
 		if ((cartridge.MM_ROM[0][0x0143]) == 0) {
-			System.out.println("Cartridge appears to be a GameBoy game");
+			CPULogger.log("Cartridge appears to be a GameBoy game");
 		} else {
-			System.out.println("Cartridge could be a ColorGameBoy game");
+			CPULogger.log("Cartridge could be a ColorGameBoy game");
 		}
 
 		cartridge.rom_mm_size = 0;
 		switch ((cartridge.MM_ROM[0][0x0148])) {
 		case 0x00:
 			cartridge.rom_mm_size = 2 << 2;
-			System.out.println("ROM size = 32KByte (no ROM banking)");
+			CPULogger.log("ROM size = 32KByte (no ROM banking)");
 			break;
 		case 0x01:
 			cartridge.rom_mm_size = 4 << 2;
-			System.out.println("ROM size = 64KByte (4 banks)");
+			CPULogger.log("ROM size = 64KByte (4 banks)");
 			break;
 		case 0x02:
 			cartridge.rom_mm_size = 8 << 2;
-			System.out.println("ROM size = 128KByte (8 banks)");
+			CPULogger.log("ROM size = 128KByte (8 banks)");
 			break;
 		case 0x03:
 			cartridge.rom_mm_size = 16 << 2;
-			System.out.println("ROM size = 256KByte (16 banks)");
+			CPULogger.log("ROM size = 256KByte (16 banks)");
 			break;
 		case 0x04:
 			cartridge.rom_mm_size = 32 << 2;
-			System.out.println("ROM size = 512KByte (32 banks)");
+			CPULogger.log("ROM size = 512KByte (32 banks)");
 			break;
 		case 0x05:
 			cartridge.rom_mm_size = 64 << 2;
-			System.out.println("ROM size = 1MByte (64 banks) - only 63 banks used by MBC1");
+			CPULogger.log("ROM size = 1MByte (64 banks) - only 63 banks used by MBC1");
 			break;
 		case 0x06:
 			cartridge.rom_mm_size = 128 << 2;
-			System.out.println("ROM size = 2MByte (128 banks) - only 125 banks used by MBC1");
+			CPULogger.log("ROM size = 2MByte (128 banks) - only 125 banks used by MBC1");
 			break;
 		case 0x07:
 			cartridge.rom_mm_size = 256 << 2;
-			System.out.println("ROM size = 4MByte (256 banks)");
+			CPULogger.log("ROM size = 4MByte (256 banks)");
 			break;
 		case 0x08:
 			cartridge.rom_mm_size = 512 << 2;
-			System.out.println("ROM size = 8MByte (512 banks)");
+			CPULogger.log("ROM size = 8MByte (512 banks)");
 			break;
 		case 0x52:
 			cartridge.rom_mm_size = 72 << 2;
-			System.out.println("ROM size = 1.1MByte (72 banks)");
+			CPULogger.log("ROM size = 1.1MByte (72 banks)");
 			break;
 		case 0x53:
 			cartridge.rom_mm_size = 80 << 2;
-			System.out.println("ROM size = 1.2MByte (80 banks)");
+			CPULogger.log("ROM size = 1.2MByte (80 banks)");
 			break;
 		case 0x54:
 			cartridge.rom_mm_size = 96 << 2;
-			System.out.println("ROM size = 1.5MByte (96 banks)");
+			CPULogger.log("ROM size = 1.5MByte (96 banks)");
 			break;
 		default:
 			System.out.printf("WARNING: Non-standard ROM size! (MM_ROM[0][0x0148]=0x%02x=%d)\n", (cartridge.MM_ROM[0][0x0148]), (cartridge.MM_ROM[0][0x0148]));
@@ -252,23 +252,23 @@ public class CartridgeController {
 		switch ((cartridge.MM_ROM[0][0x0149])) {
 		case 0x00:
 			cartridge.ram_mm_size = 0;
-			System.out.println(cartridge.MBC == 2 ? "Cartridge has 512x4 bits of RAM" : "Cartridge has no RAM");
+			CPULogger.log(cartridge.MBC == 2 ? "Cartridge has 512x4 bits of RAM" : "Cartridge has no RAM");
 			break;
 		case 0x01:
 			cartridge.ram_mm_size = 1;
-			System.out.println("Cartridge has 2KBytes of RAM");
+			CPULogger.log("Cartridge has 2KBytes of RAM");
 			break;
 		case 0x02:
 			cartridge.ram_mm_size = 2;
-			System.out.println("Cartridge has 8Kbytes of RAM");
+			CPULogger.log("Cartridge has 8Kbytes of RAM");
 			break;
 		case 0x03:
 			cartridge.ram_mm_size = 8;
-			System.out.println("Cartridge has 32 KBytes of RAM (4 banks of 8KBytes each)");
+			CPULogger.log("Cartridge has 32 KBytes of RAM (4 banks of 8KBytes each)");
 			break;
 		case 0x04:
 			cartridge.ram_mm_size = 32;
-			System.out.println("Cartridge has 128 KBytes of RAM (16 banks of 8KBytes each)");
+			CPULogger.log("Cartridge has 128 KBytes of RAM (16 banks of 8KBytes each)");
 			break;
 		default:
 			System.out.printf("WARNING: Non-standard RAM size! (MM_ROM[0][0x0149]=0x%02x=%d)\n", (cartridge.MM_ROM[0][0x0149]), (cartridge.MM_ROM[0][0x0149]));
@@ -284,7 +284,7 @@ public class CartridgeController {
 				break;
 			cartridge.title += (char) (cartridge.MM_ROM[0][0x0134 + i]);
 		}
-		System.out.println("ROM Name appears to be `" + cartridge.title + "'");
+		CPULogger.log("ROM Name appears to be `" + cartridge.title + "'");
 
 		System.out.printf("Trying to load " + (cartridge.rom_mm_size >> 2) + " banks of ROM ");
 
