043e40ea2bc2d4c6a83e7fd4056e5feadddab995
seth.holth <seth.holth@c3224f74-1d76-11df-a646-7b274f648fea>
2010-08-02 09:00:29 +0000

    Deleting unused branches

--- branches/stable-0.0.2/jgbe/src/com/arykow/applications/ugabe/client/UGABEServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.arykow.applications.ugabe.client;
-
-import java.util.List;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface UGABEServiceAsync {
-	public abstract void loadCartridge(String fileName, AsyncCallback<List<Integer>> callback);
-}
